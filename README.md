932ceb306a958c9d227d33cbcacb3d689f74d863
Hasan Zahid <hasanthedev@gmail.com>
2024-11-04 18:56:02 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## React + TypeScript + Vite
+
+### This is a project that is used to refresh my knowledge of React, TypeScript and Vite, Vitest etc.
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { ProjectsPage } from "./projects/ProjectsPage";
+
+function App() {
+  return (
+    <>
+      <div className="Container">
+        <ProjectsPage />
+      </div>
+    </>
+  );
+}
+
+export default App;
