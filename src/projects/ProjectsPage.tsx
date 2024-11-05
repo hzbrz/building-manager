433d6898ab59939015617030a2b134df7f10417a
HZ <hasanthedev@gmail.com>
2024-11-04 20:28:13 -0500

    added mock data and rendering as json

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,3 +1,10 @@
+import { MOCK_PROJECTS } from "./MockProjects";
+
 export const ProjectsPage = () => {
-  return <h1>Projects</h1>;
+  return (
+    <>
+      <h1>Projects</h1>
+      <pre>{JSON.stringify(MOCK_PROJECTS, null, "")}</pre>
+    </>
+  );
 };
